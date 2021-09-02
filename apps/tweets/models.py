dac84b9f092f07d160dad3e16eff9754bd398fb0
bamboo-nova <suzuk1nov11@gmail.com>
2021-09-02 18:59:26 +0900

    implementing model

--- apps/tweets/models.py
+++ apps/tweets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweets(models.Model):
+    id = models.AutoField('tweet ID', serialize=False, serialize=False, auto_created=True, primary_key=True)
+    source = models.CharField('デバイス', )
